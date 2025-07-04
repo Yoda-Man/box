86c47d48bdeeb5dce92102841ee720d7f4c9a6b0
Yoda-Man <trustaldo@gmail.com>
2025-07-04 07:09:49 +0200

    storeName

--- lib/helper/boxx_web.dart
+++ lib/helper/boxx_web.dart
@@ -40,8 +40,8 @@ class BoxxHelper implements BoxxInterface {
       onUpgradeNeeded: (VersionChangeEvent e) {
         // Create object stores (tables) if they don't exist
         final db = (e.target as OpenDBRequest).result;
-        if (!db.objectStoreNames.contains('myStore')) {
-          db.createObjectStore('myStore', keyPath: 'id');
+        if (!db.objectStoreNames.contains(storeName)) {
+          db.createObjectStore(storeName, keyPath: 'id');
         }
       },
     );
